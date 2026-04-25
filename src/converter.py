ce31474c94cf92f91a48868c61edd1c0e488347b
Julia <julia.studentka@uczelnia.pl>
2026-04-25 18:49:16 +0000

    Formatowanie kodu pod rygorystyczne zasady PEP8

--- src/converter.py
+++ src/converter.py
@@ -1,11 +1,14 @@
-def celsius_to_fahrenheit(c): 
-    return (c * 9/5) + 32
+def celsius_to_fahrenheit(c):
+    return (c * 9 / 5) + 32
 
-def fahrenheit_to_celsius(f): 
-    return (f - 32) * 5/9
 
-def miles_to_km(m): 
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5 / 9
+
+
+def miles_to_km(m):
     return m * 1.60934
 
-def km_to_miles(k): 
+
+def km_to_miles(k):
     return k / 1.60934
